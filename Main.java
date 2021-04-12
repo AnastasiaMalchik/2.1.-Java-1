f6ffd24b6baed93cc3ba39a444da9d49f100c339
Anastasia Malchik <info@myspshop.ru>
2021-04-13 00:53:12 +0500

    Commit 1

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 1000;
+        int milePrice = 20;
+        int miles = price/milePrice;
+        System.out.println("Miles = " + miles);
+    }
+}
